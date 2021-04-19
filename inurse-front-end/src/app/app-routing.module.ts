96f9f4fe7d08c78e2bee11205016cf8b6d02d0ba
Pol Reig <pol.reigp@e-campus.uab.cat>
2021-04-19 11:26:40 +0200

    Canvi enrutador Visita

--- inurse-front-end/src/app/app-routing.module.ts
+++ inurse-front-end/src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'visita',
+    loadChildren: () => import('./visita/visita.module').then( m => m.VisitaPageModule)
   }
 ];
 
