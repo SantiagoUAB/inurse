f1621fe7a35d32b0c6e4b3c9c945467929bc2447
SantiUAB-campus <santiagodaniel.rodriguez@e-campus.uab.cat>
2021-04-21 13:03:30 +0200

    add http get ok

--- inurse-front-end/src/app/ficha-paciente/ficha-paciente.page.ts
+++ inurse-front-end/src/app/ficha-paciente/ficha-paciente.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {PacientesService} from '../service/pacientes.service';
 import {IPatient} from '../models/interface.patient.model';
 import {Patient} from '../class/class.patient';
+import {fakeAsync} from '@angular/core/testing';
 
 @Component({
   selector: 'app-ficha-paciente',
@@ -11,11 +12,13 @@ import {Patient} from '../class/class.patient';
 export class FichaPacientePage implements OnInit {
   private patient: Patient;
   private urlImg: string;
+  private dataLoad: boolean
 
 
   constructor(private pacienteService: PacientesService) {
     this.urlImg = 'https://thispersondoesnotexist.com/image';
     this.urlImg = 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg';
+    this.dataLoad = false;
 
   }
 
@@ -25,6 +28,7 @@ export class FichaPacientePage implements OnInit {
       console.log('En ficha paciente');
       console.log(data);
       this.patient = new Patient(data);
+      this.dataLoad = true;
 
 
     });
