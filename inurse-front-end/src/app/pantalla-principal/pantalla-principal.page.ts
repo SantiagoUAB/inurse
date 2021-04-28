064e1de00318a1670d99806282fa9b2ba61ff5ca
SantiUAB-campus <santiagodaniel.rodriguez@e-campus.uab.cat>
2021-04-28 12:10:23 +0200

    fix pantalla principal Menu and list room

--- inurse-front-end/src/app/pantalla-principal/pantalla-principal.page.ts
+++ inurse-front-end/src/app/pantalla-principal/pantalla-principal.page.ts
@@ -8,57 +8,58 @@ import{Floor} from '../class/class.floor';
   styleUrls: ['./pantalla-principal.page.scss'],
 })
 export class PantallaPrincipalPage implements OnInit {
-  
+
   private arrayPlanta: Array<any>;
   private resultado: Array<any>;
   private planta: Floor;
-  private aux: Array<any>;
+  private roomPacientes: Array<any>;
   private aux2: string[];
   private habitacion:String;
   private lista: any[];
   private listaPaciente: any[];
- 
+
   constructor(private plantasService:PlantasService) {
-    
+
     this.plantasService.getPlantas().subscribe(data=>{
       console.log('Plantas');
       console.log(data);
       this.planta= new Floor(data);
       this.arrayPlanta= this.planta.results;
-      
+
     });
 
     this.plantasService.getPaciente().subscribe(data=>{
       console.log('Pacientes');
       console.log(data);
-      this.aux=data['results'];
-      
-      console.log(this.aux);
+      this.roomPacientes=data['results'];
+
+      console.log(this.roomPacientes);
+
+      this.cambioPlanta(1)
     });
 
 
 
-    
+
 console.log(this.resultado);
 
 
    }
   cambioPlanta(planta){
     this.lista=[];
-    for(let file of this.aux) {
+    for(let file of this.roomPacientes) {
       if(planta==(file['room']['floor']['floor_num'])){
       this.aux2=[file['first_name'],file['room']['room_num']];
       this.lista.push(this.aux2);
-      
       }
-      
+
    }
    this.listaPaciente=this.lista;
    console.log(this.listaPaciente);
 
 
 
-    
+
 }
   ngOnInit() {
   }
