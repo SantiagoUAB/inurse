28643fa5f243ff54f6733b2f215bd44dd453afaa
Pol Reig <pol.reigp@e-campus.uab.cat>
2021-04-30 20:42:08 +0200

    Login y logout (subidos correctamente)

---
+++ inurse-front-end/src/app/login/login.page.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  username = '';
+  password = '';
+  constructor(
+    public toastController: ToastController,
+    private router: Router,
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+  }
+  async proceedLogin() {
+    if (this.username === '') {
+      const toast = await this.toastController.create({
+        message: 'Por favor, introduce el usuario',
+        duration: 2000
+      });
+      toast.present();
+    } else if (this.password === '') {
+      const toast = await this.toastController.create({
+        message: 'Por favor, introduce la contraseña',
+        duration: 2000
+      });
+      toast.present();
+    }
+    this.sendPostRequest();
+    // this.getDataPatientFile();
+  }
+
+  async sendPostRequest() {
+    const postData = { username: this.username, password: this.password };
+    this.httpClient.post('http://158.109.74.51:55001/auth/login/', postData).subscribe(data => {
+      console.log(data);
+      console.log('login correcto');
+      this.comprovationUser();
+      this.router.navigate(['/pantalla-principal']);
+    }, error => {
+      this.errorUserPassword();
+      console.log('login incorrecto');
+    });
+  }
+
+
+  /*async getDataPatientFile() {
+    this.userService.getUser(this.username).subscribe(data => {
+      console.log(data['count']);
+      if (data['count'] === 0) {
+        this.errorUserPassword();
+        return;
+      }
+      else {
+        this.user = new User(data);
+        console.log(this.user.username);
+        this.comprovationUser();
+      }
+    });
+  }*/
+  async comprovationUser(){
+    const toast = await this.toastController.create({
+      message: 'Has iniciado sesión correctamente',
+      duration: 2000
+    });
+    toast.present();
+  }
+  async errorUserPassword() {
+    const toast = await this.toastController.create({
+      message: 'Usuario o contaseña incorrectos',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+}
---
+++ inurse-front-end/src/app/logout/logout.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.page.html',
+  styleUrls: ['./logout.page.scss'],
+})
+export class LogoutPage implements OnInit {
+  constructor(
+    public toastController: ToastController,
+    private router: Router,
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.logout();
+  }
+
+  async logout() {
+    const postData = {content: ''};
+    this.httpClient.post('http://158.109.74.51:55001/auth/logout/', postData).subscribe(data => {
+      console.log(data);
+      console.log('logout correcto');
+      this.comprovationUser();
+      this.router.navigate(['/login']);
+    }, error => {
+      this.errorUserPassword();
+      console.log('logout incorrecto');
+    });
+  }
+
+  async comprovationUser(){
+    const toast = await this.toastController.create({
+      message: 'Has cerrado sesión',
+      duration: 2000
+    });
+    toast.present();
+  }
+  async errorUserPassword() {
+    const toast = await this.toastController.create({
+      message: 'No has podido cerrar sesión',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+}
