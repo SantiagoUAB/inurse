2fe3eb9a41a1721ac3a6e6efe13c14e89110d06b
SantiUAB <santiagodaniel.rodriguez@e-campus.uab.cat>
2021-05-21 22:10:13 +0200

    add button back in Page Visit add Progress Bar in Page Visit

---
+++ inurse-front-end/src/app/class/class.manageErrors.ts
@@ -0,0 +1,19 @@
+import {HttpErrorResponse} from "@angular/common/http";
+import {throwError} from "rxjs";
+
+export class ClassManageErrors{
+
+
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
--- inurse-front-end/src/app/visita/visita.page.ts
+++ inurse-front-end/src/app/visita/visita.page.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PacientesService} from '../service/pacientes.service';
 import {Router} from '@angular/router';
 import {Visit} from '../class/class.visit';
 import {Patient} from '../class/class.patient';
-import {HttpClient} from '@angular/common/http';
-import { ToastController } from '@ionic/angular';
-import {FichaPacientePage} from '../ficha-paciente/ficha-paciente.page';
+import {HttpClient, HttpEvent, HttpEventType} from '@angular/common/http';
+import {ToastController} from '@ionic/angular';
 import {PantallaPrincipalPage} from '../pantalla-principal/pantalla-principal.page';
+import {catchError} from "rxjs/operators";
+import {ClassManageErrors} from '../class/class.manageErrors';
 
 @Component({
   providers: [PantallaPrincipalPage],
@@ -24,12 +25,18 @@ export class VisitaPage implements OnInit {
   treatment: string;
   treatmentAux: string;
 
+  manageErrors: ClassManageErrors;
+  progress: number;
+
   constructor(private patientSevice: PacientesService,
               private router: Router,
               private httpClient: HttpClient,
-              public toastController: ToastController,
-              public pantallaPrincipal: PantallaPrincipalPage
-  ) { }
+              public toastController: ToastController
+
+  ) {
+    this.manageErrors = new ClassManageErrors();
+    this.progress = 0;
+  }
 
   ngOnInit() {
     this.idVisita = this.patientSevice.getIdVisita();
@@ -63,21 +70,40 @@ export class VisitaPage implements OnInit {
     console.log('TreatmentAux: ' + this.treatmentAux);
     if (this.treatment !== this.treatmentAux) {
       // const postData = { treatment: this.treatment };
-      const postData = { date: this.visit['data'], treatment: this.treatment, patient: '1' };
-      console.log(postData);
-      this.httpClient.post('http://158.109.74.51:55001/appointment/', postData).subscribe(async data => {
-        console.log(data);
-        console.log('Visita modificada correctamente');
-        this.correctModification();
-        // this.fichaPaciente.getDataPatientFile();
-        // this.fichaPaciente.ngOnInit();
-        
-        await this.goToFichaPaciente();
+      const postData = { date: this.visit['data'], treatment: this.treatment, patient: this.idPaciente };
+      console.log('enio la visita', postData);
+      this.httpClient.post('http://158.109.74.51:55001/appointment/', postData, {
+        reportProgress: true,
+        observe: 'events'})
+        .pipe(
+          catchError(this.manageErrors.errorMgmt)
+        )
+        .subscribe(async (event: HttpEvent<any>) => {
+          // console.log(data);
+          switch (event.type){
+            case HttpEventType.Sent:
+              console.log('Request send');
+              break;
+            case HttpEventType.ResponseHeader:
+              console.log('Response header has ben received!', event.headers);
+              break;
+            case HttpEventType.UploadProgress:
+              this.progress = Math.round(event.loaded / event.total * 100);
+              console.log(`Progress ... ${this.progress} %`);
+              break;
+            case HttpEventType.Response:
+              console.log('New Appointment created! ', event.body);
+              setTimeout(() => {
+              this.progress = 0;
+            }, 1500);
+          }
+          // console.log('Visita modificada correctamente');
+          this.correctModification();
 
-
-      }, error => {
-        console.log('No se ha podido modificar la visita');
-      });
+          await this.goToFichaPaciente();
+        }, error => {
+          console.log('No se ha podido modificar la visita');
+        });
     }
     else {
       this.errorModification();
@@ -108,8 +134,24 @@ export class VisitaPage implements OnInit {
     toast.present();
   }
 
+  private progressResponse( event: HttpEvent<any>) {
+    switch (event.type){
+      case HttpEventType.Sent:
+        console.log('Request send');
+        break;
+      case HttpEventType.ResponseHeader:
+        console.log('Response header has ben received!', event.headers);
+        break;
+      case HttpEventType.UploadProgress:
+        this.progress = Math.round(event.loaded / event.total * 100);
+        console.log(`Progress ... ${this.progress} %`);
+        break;
+      case HttpEventType.Response:
+        console.log('New Appointment created! ', event.body);
+        setTimeout(() => {
+          this.progress = 0;
+        }, 1500);
+    }
+  }
 }
 
-function delay(ms: number) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
-}
