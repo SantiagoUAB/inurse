504f047a2f462002951dc54f743aec931725af5b
SantiUAB <santiagodaniel.rodriguez@e-campus.uab.cat>
2021-05-22 09:56:20 +0200

    Generate APK release

---
+++ inurse-front-end/android/app/build/intermediates/merged_assets/release/out/public/pantalla-principal-pantalla-principal-module.js
@@ -0,0 +1,83 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["pantalla-principal-pantalla-principal-module"],{
+
+/***/ "nWdj":
+/*!*****************************************************************!*\
+  !*** ./src/app/pantalla-principal/pantalla-principal.module.ts ***!
+  \*****************************************************************/
+/*! exports provided: PantallaPrincipalPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PantallaPrincipalPageModule", function() { return PantallaPrincipalPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "mrSG");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "3Pt+");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "TEn/");
+/* harmony import */ var _pantalla_principal_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./pantalla-principal-routing.module */ "wtX7");
+/* harmony import */ var _pantalla_principal_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./pantalla-principal.page */ "qB8f");
+
+
+
+
+
+
+
+let PantallaPrincipalPageModule = class PantallaPrincipalPageModule {
+};
+PantallaPrincipalPageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [
+            _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"],
+            _pantalla_principal_routing_module__WEBPACK_IMPORTED_MODULE_5__["PantallaPrincipalPageRoutingModule"]
+        ],
+        declarations: [_pantalla_principal_page__WEBPACK_IMPORTED_MODULE_6__["PantallaPrincipalPage"]]
+    })
+], PantallaPrincipalPageModule);
+
+
+
+/***/ }),
+
+/***/ "wtX7":
+/*!*************************************************************************!*\
+  !*** ./src/app/pantalla-principal/pantalla-principal-routing.module.ts ***!
+  \*************************************************************************/
+/*! exports provided: PantallaPrincipalPageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PantallaPrincipalPageRoutingModule", function() { return PantallaPrincipalPageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "mrSG");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _pantalla_principal_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./pantalla-principal.page */ "qB8f");
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _pantalla_principal_page__WEBPACK_IMPORTED_MODULE_3__["PantallaPrincipalPage"]
+    }
+];
+let PantallaPrincipalPageRoutingModule = class PantallaPrincipalPageRoutingModule {
+};
+PantallaPrincipalPageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]],
+    })
+], PantallaPrincipalPageRoutingModule);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pantalla-principal-pantalla-principal-module.js.map
---
+++ inurse-front-end/android/app/src/main/assets/public/logout-logout-module.js
@@ -0,0 +1,202 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["logout-logout-module"],{
+
+/***/ "6yqL":
+/*!*************************************************!*\
+  !*** ./src/app/logout/logout-routing.module.ts ***!
+  \*************************************************/
+/*! exports provided: LogoutPageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LogoutPageRoutingModule", function() { return LogoutPageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "mrSG");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _logout_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./logout.page */ "bxzc");
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _logout_page__WEBPACK_IMPORTED_MODULE_3__["LogoutPage"]
+    }
+];
+let LogoutPageRoutingModule = class LogoutPageRoutingModule {
+};
+LogoutPageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]],
+    })
+], LogoutPageRoutingModule);
+
+
+
+/***/ }),
+
+/***/ "9nES":
+/*!*******************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/logout/logout.page.html ***!
+  \*******************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("<ion-header>\r\n  <ion-toolbar>\r\n    <ion-title>logout</ion-title>\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content>\r\n\r\n</ion-content>\r\n");
+
+/***/ }),
+
+/***/ "I/pM":
+/*!*****************************************!*\
+  !*** ./src/app/logout/logout.page.scss ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJsb2dvdXQucGFnZS5zY3NzIn0= */");
+
+/***/ }),
+
+/***/ "bxzc":
+/*!***************************************!*\
+  !*** ./src/app/logout/logout.page.ts ***!
+  \***************************************/
+/*! exports provided: LogoutPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LogoutPage", function() { return LogoutPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "mrSG");
+/* harmony import */ var _raw_loader_logout_page_html__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! raw-loader!./logout.page.html */ "9nES");
+/* harmony import */ var _logout_page_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./logout.page.scss */ "I/pM");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "TEn/");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/router */ "tyNb");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @angular/common/http */ "tk/3");
+/* harmony import */ var _service_token_storage_service__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../service/token-storage.service */ "mwsN");
+/* harmony import */ var _pantalla_principal_pantalla_principal_page__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../pantalla-principal/pantalla-principal.page */ "qB8f");
+
+
+
+
+
+
+
+
+
+let LogoutPage = class LogoutPage {
+    constructor(toastController, router, httpClient, tokenStorage, pantallaPrincipal) {
+        this.toastController = toastController;
+        this.router = router;
+        this.httpClient = httpClient;
+        this.tokenStorage = tokenStorage;
+        this.pantallaPrincipal = pantallaPrincipal;
+    }
+    ngOnInit() {
+        this.logout();
+        this.pantallaPrincipal.desfijarPaciente();
+    }
+    logout() {
+        const postData = { content: '' };
+        this.httpClient.post('http://158.109.74.51:55001/auth/logout/', postData).subscribe(data => {
+            console.log(data);
+            console.log('logout correcto');
+            this.comprovationUser();
+            this.router.navigate(['/login']);
+            this.tokenStorage.sinOut();
+        }, error => {
+            this.errorUserPassword();
+            console.log('logout incorrecto');
+            this.router.navigate(['/login']);
+        });
+    }
+    comprovationUser() {
+        return Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__awaiter"])(this, void 0, void 0, function* () {
+            const toast = yield this.toastController.create({
+                message: 'Has cerrado sesión',
+                duration: 2000
+            });
+            toast.present();
+        });
+    }
+    errorUserPassword() {
+        return Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__awaiter"])(this, void 0, void 0, function* () {
+            const toast = yield this.toastController.create({
+                message: 'No has podido cerrar sesión',
+                duration: 2000
+            });
+            toast.present();
+        });
+    }
+};
+LogoutPage.ctorParameters = () => [
+    { type: _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["ToastController"] },
+    { type: _angular_router__WEBPACK_IMPORTED_MODULE_5__["Router"] },
+    { type: _angular_common_http__WEBPACK_IMPORTED_MODULE_6__["HttpClient"] },
+    { type: _service_token_storage_service__WEBPACK_IMPORTED_MODULE_7__["TokenStorageService"] },
+    { type: _pantalla_principal_pantalla_principal_page__WEBPACK_IMPORTED_MODULE_8__["PantallaPrincipalPage"] }
+];
+LogoutPage = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_3__["Component"])({
+        providers: [_pantalla_principal_pantalla_principal_page__WEBPACK_IMPORTED_MODULE_8__["PantallaPrincipalPage"]],
+        selector: 'app-logout',
+        template: _raw_loader_logout_page_html__WEBPACK_IMPORTED_MODULE_1__["default"],
+        styles: [_logout_page_scss__WEBPACK_IMPORTED_MODULE_2__["default"]]
+    })
+], LogoutPage);
+
+
+
+/***/ }),
+
+/***/ "q6at":
+/*!*****************************************!*\
+  !*** ./src/app/logout/logout.module.ts ***!
+  \*****************************************/
+/*! exports provided: LogoutPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LogoutPageModule", function() { return LogoutPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "mrSG");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "ofXK");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "3Pt+");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @ionic/angular */ "TEn/");
+/* harmony import */ var _logout_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./logout-routing.module */ "6yqL");
+/* harmony import */ var _logout_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./logout.page */ "bxzc");
+
+
+
+
+
+
+
+let LogoutPageModule = class LogoutPageModule {
+};
+LogoutPageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [
+            _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+            _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"],
+            _logout_routing_module__WEBPACK_IMPORTED_MODULE_5__["LogoutPageRoutingModule"]
+        ],
+        declarations: [_logout_page__WEBPACK_IMPORTED_MODULE_6__["LogoutPage"]]
+    })
+], LogoutPageModule);
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=logout-logout-module.js.map
